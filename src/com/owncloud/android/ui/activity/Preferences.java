e1716df41c7dfb1cd253e588b1b0e009af5f2c79
Luke Owncloud <owncloud@ohrt.org>
2016-04-26 09:40:31 +0200

    configure account for instant upload

--- src/com/owncloud/android/ui/activity/Preferences.java
+++ src/com/owncloud/android/ui/activity/Preferences.java
@@ -797,6 +797,7 @@ public class Preferences extends PreferenceActivity
         SharedPreferences.Editor editor = appPrefs.edit();
         editor.putString(PreferenceKeys.INSTANT_UPLOAD_PATH, mUploadPath);
                 .putString(INSTANT_UPLOAD_PATH_ACCOUNT, mUploadPathAccount);
+        editor.putString("instant_upload_path_account", mUploadPathAccount);
         editor.commit();
     }
 
@@ -821,6 +822,7 @@ public class Preferences extends PreferenceActivity
         SharedPreferences.Editor editor = appPrefs.edit();
         editor.putString(PreferenceKeys.INSTANT_VIDEO_UPLOAD_PATH, mUploadVideoPath);
                 .putString(INSTANT_VIDEO_UPLOAD_PATH_ACCOUNT, mUploadVideoPathAccount);
+        editor.putString("instant_video_upload_path_account", mUploadVideoPathAccount);
         editor.commit();
     }
 
